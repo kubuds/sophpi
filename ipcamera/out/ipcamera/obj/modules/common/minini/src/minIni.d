ee81ded34d7e22c71c37c64d2cf3d678949f292f
yao.lu <yao.lu@sophgo.com>
2024-09-03 14:50:17 +0800

    add the ipcamera project

---
+++ ipcamera/out/ipcamera/obj/modules/common/minini/src/minIni.d
@@ -0,0 +1,4 @@
+/mnt/c/sophpi/ipcamera/out/ipcamera/obj/modules/common/minini/src/minIni.o: \
+ /mnt/c/sophpi/ipcamera/modules/common/minini/src/minIni.c \
+ /mnt/c/sophpi/ipcamera/modules/common/include/minIni.h \
+ /mnt/c/sophpi/ipcamera/modules/common/include/minGlue.h
