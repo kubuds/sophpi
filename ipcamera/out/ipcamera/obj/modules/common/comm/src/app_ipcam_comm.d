5a8e3342d44791dbdcb2a5d5a1e13f996795d0f5
yao.lu <yao.lu@sophgo.com>
2024-09-04 19:38:04 +0800

    add the ipcamera project

---
+++ ipcamera/out/ipcamera/obj/modules/common/comm/src/app_ipcam_comm.d
@@ -0,0 +1,3 @@
+/mnt/c/sophpi/ipcamera/out/ipcamera/obj/modules/common/comm/src/app_ipcam_comm.o: \
+ /mnt/c/sophpi/ipcamera/modules/common/comm/src/app_ipcam_comm.c \
+ /mnt/c/sophpi/ipcamera/modules/common/include/app_ipcam_comm.h
---
+++ ipcamera/out/ipcamera/obj/modules/common/list/src/app_ipcam_ll.d
@@ -0,0 +1,3 @@
+/mnt/c/sophpi/ipcamera/out/ipcamera/obj/modules/common/list/src/app_ipcam_ll.o: \
+ /mnt/c/sophpi/ipcamera/modules/common/list/src/app_ipcam_ll.c \
+ /mnt/c/sophpi/ipcamera/modules/common/include/app_ipcam_ll.h
